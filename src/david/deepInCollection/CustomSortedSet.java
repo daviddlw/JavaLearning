cee52b8cba46ea46c0dd42b504c6598e2d48166b
daviddlw <davidjefiny@gmail.com>
2013-10-11 11:48:12 +0800

    测试提交

---
+++ src/david/deepInCollection/CustomSortedSet.java
@@ -0,0 +1,5 @@
+package david.deepInCollection;
+
+public class CustomSortedSet {
+	
+}
--- src/david/deepInCollection/MainFunction.java
+++ src/david/deepInCollection/MainFunction.java
@@ -4,15 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainFunction {
-    public static void main(String[] args) {
-	// DemoRun.fillList();
-	// DemoRun.collectinoDataDemo();
-	// DemoRun.randomGen();
-	// DemoRun.countIntegerDemo();
-	// DemoRun.mapEntryDemo();
-	// CollectionMethods.collectionMethods();
-	// CollectionMethods.iteratorDemo();
-	// CollectionMethods.slistDemo();
-	DemoRun.treeSetDemo();
-    }
+	public static void main(String[] args) {
+		// DemoRun.fillList();
+		// DemoRun.collectinoDataDemo();
+		// DemoRun.randomGen();
+		// DemoRun.countIntegerDemo();
+		// DemoRun.mapEntryDemo();
+		// CollectionMethods.collectionMethods();
+		// CollectionMethods.iteratorDemo();
+		// CollectionMethods.slistDemo();
+		DemoRun.treeSetDemo();
+	}
 }
