2b712ffadb2a8a2347655558875c0cce7f51d6da
daviddlw <davidjefiny@gmail.com>
2013-10-28 17:30:42 +0800

    grud check in~

--- src/david/jdbc/JDBCTest.java
+++ src/david/jdbc/JDBCTest.java
@@ -1,8 +1,8 @@
 package david.jdbc;
 
-import java.security.interfaces.RSAKey;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,68 +14,146 @@ import com.mysql.jdbc.Statement;
 import david.model.Student;
 
 public class JDBCTest {
-    private static Connection conn;
-    private static Statement st;
-    private static final String CONNECTION_DRIVER_STR = "com.mysql.jdbc.Driver";
-    private static final String CONNECTION_STR = "jdbc:mysql://localhost:3306/test"; // url规则=>
-										     // 协议+IP地址（域名）+端口+数据库名
+	private static Connection conn;
+	private static Statement st;
+	private static PreparedStatement pst = null;
+	private static final String CONNECTION_DRIVER_STR = "com.mysql.jdbc.Driver";
+	private static final String CONNECTION_STR = "jdbc:mysql://localhost:3306/test"; // url规则=>
+	// 协议+IP地址（域名）+端口+数据库名
 
-    public static void insert(Student item) {
-	conn = getConnection();
-	String sql = String
-		.format("insert into student (name, createtime) values ('%s', sysdate())",
-			item.getName());
-	try {
-	    st = (Statement) conn.createStatement();
-	    int count = st.executeUpdate(sql);
-	    if (count > 0)
-		System.out.println("添加成功!");
+	/*
+	 * 没有使用sql参数化的插入
+	 */
+	public static void insert(Student item) {
+		conn = getConnection();
+		String sql = String
+				.format("insert into student (name, createtime) values ('%s', sysdate())",
+						item.getName());
+		try {
+			st = (Statement) conn.createStatement();
+			int count = st.executeUpdate(sql);
+			if (count > 0)
+				System.out.println("添加成功!");
 
-	    conn.close();
-	} catch (Exception e) {
-	    // TODO: handle exception
-	    System.out.println(e.getMessage());
-	}
-    }
-
-    public static void query() {
-	List<Student> students = new ArrayList<Student>();
-	conn = getConnection();
-	String sql = "select * from student";
-	try {
-	    st = (Statement) conn.createStatement();
-	    ResultSet rs = st.executeQuery(sql);
-	    while (rs.next()) {
-		int id = rs.getInt("id");
-		String name = rs.getString("name");
-		students.add(new Student(id, name));
-	    }
-	    conn.close();
-	} catch (Exception ex) {
-	    // TODO: handle exception
-	    System.out.println(ex.getMessage());
+			conn.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
 	}
 
-	for (Student student : students) {
-	    System.out.println(student);
-	}
-    }
+	/*
+	 * 删除某个查询对象
+	 */
+	public static void delete(int id) {
+		conn = getConnection();
+		String sql = "delete from student where id = ?";
+		try {
+			conn.setAutoCommit(false);
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			int count = pst.executeUpdate();
+			conn.commit();
 
-    /*
-     * 获取数据库访问链接
-     */
-    private static Connection getConnection() {
-	Connection conn = null;
-	try {
-	    Class.forName(CONNECTION_DRIVER_STR);
-	    conn = DriverManager.getConnection(CONNECTION_STR, "root",
-		    "david0110");
-	} catch (Exception ex) {
-	    // TODO: handle exception
-	    StrBuilder sb = new StrBuilder("数据库连接失败了");
-	    sb.appendln(ex.getMessage());
-	    System.out.println(sb.toString());
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
 	}
-	return conn;
-    }
+
+	/*
+	 * 更新某个查询对象
+	 */
+	public static void update(Student student) {
+		conn = getConnection();
+		String sql = "update student set name=? where id=?";
+		try {
+			conn.setAutoCommit(false);
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, student.getName());
+			pst.setInt(2, student.getId());
+			int count = pst.executeUpdate();
+			conn.commit();
+			if (count == 0)
+				System.out.println("没有查询到相关Id的信息");
+
+			conn.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/*
+	 * 根据Id获取Student对象
+	 */
+	public static Student getStudentById(int id) {
+		conn = getConnection();
+		String sql = "select id, name from student where id = ?";
+		Student student = new Student();
+		try {
+			conn.setAutoCommit(false);
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+
+			while (rs.next()) {
+				student.setId(id);
+				student.setName(rs.getString("name"));
+			}
+
+			if (student.getId() == 0)
+				System.out.println("查无匹配");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+
+		return student;
+	}
+
+	/*
+	 * 查询student列表
+	 */
+	public static void queryList() {
+		List<Student> students = new ArrayList<Student>();
+		conn = getConnection();
+		String sql = "select * from student";
+		try {
+			st = (Statement) conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				students.add(new Student(id, name));
+			}
+			conn.close();
+		} catch (Exception ex) {
+			// TODO: handle exception
+			System.out.println(ex.getMessage());
+		}
+
+		for (Student student : students) {
+			System.out.println(student);
+		}
+	}
+
+	/*
+	 * 获取数据库访问链接
+	 */
+	private static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName(CONNECTION_DRIVER_STR);
+			conn = DriverManager.getConnection(CONNECTION_STR, "root",
+					"david0110");
+		} catch (Exception ex) {
+			// TODO: handle exception
+			StrBuilder sb = new StrBuilder("数据库连接失败了");
+			sb.appendln(ex.getMessage());
+			System.out.println(sb.toString());
+		}
+		return conn;
+	}
 }
--- src/david/jdbc/MainFunction.java
+++ src/david/jdbc/MainFunction.java
@@ -7,26 +7,89 @@ import david.util.StringUtil;
 
 public class MainFunction {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	JDBCTest.query();
-	insertStudent();
-    }
-
-    private static void insertStudent() {
-	Scanner scanner = new Scanner(System.in);
-	String s;
-	int count = 0;
-	// Student student = new Student(6, "redis");
-	Student student = new Student();
-	while (count < 1) {
-	    System.out.println("请输入您的名字：");
-	    s = scanner.next();
-	    student.setName(s);
-	    count++;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JDBCTest.queryList();
+		// insertStudent();
+		// updateStudent();
+		// deleteStudent();
+		queryStudent();
 	}
-	JDBCTest.insert(student);
-	StringUtil.printSplitLines();
-	JDBCTest.query();
-    }
+
+	/*
+	 * 插入指定学生信息
+	 */
+	private static void insertStudent() {
+		Scanner scanner = new Scanner(System.in);
+		String s;
+		int count = 0;
+		// Student student = new Student(6, "redis");
+		Student student = new Student();
+		while (count < 1) {
+			System.out.println("请输入您的名字：");
+			s = scanner.next();
+			student.setName(s);
+			count++;
+		}
+		JDBCTest.insert(student);
+		StringUtil.printSplitLines();
+		JDBCTest.queryList();
+	}
+
+	/*
+	 * 插入指定学生信息
+	 */
+	private static void deleteStudent() {
+		Scanner scanner = new Scanner(System.in);
+		int id = 0;
+		int count = 0;
+		while (count < 1) {
+			System.out.println("请输入要删除的ID：");
+			id = Integer.valueOf(scanner.next());
+			count++;
+		}
+		JDBCTest.delete(id);
+		StringUtil.printSplitLines();
+		JDBCTest.queryList();
+	}
+
+	/*
+	 * 插入指定学生信息
+	 */
+	private static void queryStudent() {
+		Scanner scanner = new Scanner(System.in);
+		int id = 0;
+		int count = 0;
+		while (count < 1) {
+			System.out.println("请输入要查询的ID：");
+			id = Integer.valueOf(scanner.next());
+			count++;
+		}
+		System.out.println(JDBCTest.getStudentById(id));
+	}
+
+	/*
+	 * 更新指定学生信息
+	 */
+	private static void updateStudent() {
+		Scanner scanner = new Scanner(System.in);
+		String s;
+		int count = 0;
+		Student student = new Student();
+		while (count < 2) {
+			if (count == 0) {
+				System.out.println("请输入要更新的Id：");
+				int id = Integer.valueOf(scanner.next());
+				student.setId(id);
+			} else {
+				System.out.println("请输入更新后的名字：");
+				s = scanner.next();
+				student.setName(s);
+			}
+			count++;
+		}
+		JDBCTest.update(student);
+		StringUtil.printSplitLines();
+		JDBCTest.queryList();
+	}
 }
