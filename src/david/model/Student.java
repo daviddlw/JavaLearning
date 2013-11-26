33b337fe51d7a691cda76df46252a6682c8e4802
daviddlw <davidjefiny@gmail.com>
2013-11-26 23:09:17 +0800

    myibatis interface query check in

--- src/david/model/Student.java
+++ src/david/model/Student.java
@@ -1,60 +1,62 @@
 package david.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Student {
 
-    private int id;
-    private String name;
-    private Calendar birth;
+	private int id;
+	private String name;
+	private Calendar birth;
 
-    public Student() {
-	// TODO Auto-generated constructor stub
-	this.id = 0;
-	this.name = "";
-	this.birth = Calendar.getInstance();
-    }
+	public Student() {
+		// TODO Auto-generated constructor stub
+		this.id = 0;
+		this.name = "";
+		this.birth = Calendar.getInstance();
+	}
 
-    public Student(int id, String name) {
-	// TODO Auto-generated constructor stub
-	this.id = id;
-	this.name = name;
-	this.birth = Calendar.getInstance();
+	public Student(int id, String name) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.name = name;
+		this.birth = Calendar.getInstance();
 
-    }
+	}
 
-    public Student(int id, String name, Calendar birth) {
-	this(id, name);
-	this.birth = birth;
-    }
+	public Student(int id, String name, Calendar birth) {
+		this(id, name);
+		this.birth = birth;
+	}
 
-    public void setId(int id) {
-	this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public int getId() {
-	return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setName(String name) {
-	this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-	return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Calendar getCalendar() {
-	return birth;
-    }
+	public Calendar getCalendar() {
+		return birth;
+	}
 
-    public void setCalendar(Calendar birth) {
-	this.birth = birth;
-    }
+	public void setCalendar(Calendar birth) {
+		this.birth = birth;
+	}
 
-    @Override
-    public String toString() {
-	return String.format("{Id=>%d, Name=>%s, birth=>%s}", id, name, birth
-		.getTime().toString());
-    }
+	@Override
+	public String toString() {
+		return String.format("{Id=>%d, Name=>%s, birth=>%s}", id, name,
+				new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(birth
+						.getTime()));
+	}
 }
--- src/david/myibatis/MainFunction.java
+++ src/david/myibatis/MainFunction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -12,29 +13,51 @@ import david.model.Student;
 
 public class MainFunction {
 
+    private static final String QUERY_STUDENT_BY_ID_STR = "david.mybatis.StudentMapper.queryStudentById";
+
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
+	// wayOfSqlSessionQuery();
+	wayOfInterfaceQuery();
 
-	InputStream inputStream = null;
+    }
+
+    public static void wayOfInterfaceQuery() {
+	IStudentOperation studentOperation = getSqlSession().getMapper(
+		IStudentOperation.class);
+	Student student = studentOperation.queryStudentById(3);
+	System.out.println(student.toString());
+    }
+
+    /*
+     * Sql Session查询实例方式
+     */
+    public static void wayOfSqlSessionQuery() {
+	SqlSession sqlSession = getSqlSession();
+	Student queryObj = sqlSession.selectOne(QUERY_STUDENT_BY_ID_STR, 3);
+
+	queryObj = queryObj == null ? new Student() : (Student) queryObj;
+	System.out.println(queryObj.toString());
+    }
+
+    /*
+     * 获取相应SqlSession
+     */
+    private static SqlSession getSqlSession() {
+	SqlSession sqlSession = null;
 	try {
+	    InputStream inputStream = null;
 	    String resource = "config/mybatis-config.xml";
 
 	    inputStream = Resources.getResourceAsStream(resource);
 	    // @off
-	    SqlSessionFactory ssf = new SqlSessionFactoryBuilder()
+	    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
 		    .build(inputStream);
-	    // @on
-	    SqlSession sqlSession = ssf.openSession();
-
-	    Student queryObj = sqlSession.selectOne(
-		    "david.mybatis.StudentMapper.queryStudentById", 3);
-	    queryObj = queryObj == null ? new Student() : (Student) queryObj;
-
-	    System.out.println(queryObj.toString());
-
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
+	    sqlSession = sqlSessionFactory.openSession();
+	} catch (Exception e) {
+	    // TODO: handle exception
 	    e.printStackTrace();
 	}
+	return sqlSession;
     }
 }
